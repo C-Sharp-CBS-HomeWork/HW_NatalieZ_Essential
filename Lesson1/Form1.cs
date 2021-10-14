e2b093a0cbdceb6a3c3907617152dad6b6f973d9
Zholob, Nataliia <OSWNataliiaZ@oriflame.com>
2021-10-14 18:47:50 +0300

    Lesson 1 task 4 finished

--- Lesson1/Form1.cs
+++ Lesson1/Form1.cs
@@ -12,6 +12,12 @@ namespace Lesson1
 {
     public partial class Form1 : Form
     {
+        private int figureNumber = 0;
+        private readonly Figure[] figures = { new Figure(new Point(-5, -5, "A"), new Point(0, 0, "B"), new Point(5, -5, "C")),
+                                     new Figure(new Point(-5, -5, "A"), new Point(-4, 3, "B"), new Point(3, 2, "C"), new Point(5, -8, "D")),
+                                     new Figure(new Point(-5, -5, "A"), new Point(-4, 0, "B"), new Point(0, 5, "C"), new Point(2, 2, "D"), new Point(5, -1, "E")),
+                                   };
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +26,8 @@ namespace Lesson1
         private void Form1_Load(object sender, EventArgs e)
         {
             FillAdressLables();
+            PrintFigure(figures[figureNumber++]);
+
         }
 
         private void FillAdressLables()
@@ -43,6 +51,21 @@ namespace Lesson1
             {
                 MessageBox.Show("Wrong value of sides. Only positive (>0) values are acceptable!");
             }
+        }
+
+        private void PrintFigure(Figure figure)
+        {
+            lblFigure.Text = figure.ToString();
+            lblPoints.Text = figure.GetPointsInfo();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if (figureNumber == figures.Length)
+            {
+                figureNumber = 0;
+            }
+            PrintFigure(figures[figureNumber++]);
         }
     }
 }
